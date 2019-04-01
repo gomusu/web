f735e8c81bc06b8d223ecb6b3492678b0b90b75d
Pavel Studeny <studeny@avast.com>
2019-04-01 09:36:18 +0200

    switch from vuetify default content to gomusu

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,28 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+      <img
+        :src="require('./assets/logo.png')"
+        height="16px"
+      />
+        <span>Gomusu</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <Contributor/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Contributor from './components/Contributor'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Contributor
   },
   data () {
     return {
---
+++ src/components/Contributor.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex xs12>
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Gomusu Rocks!
+        </h1>
+        <p class="subheading font-weight-regular">
+          Just wait to see it
+        </p>
+      </v-flex>
+
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+    })
+  }
+</script>
+
+<style>
+
+</style>
